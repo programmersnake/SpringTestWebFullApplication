44ef6b5a0bc515f4e23e4dc3ef245e2e332e5c74
Denis <45206633+programmersnake@users.noreply.github.com>
2020-06-11 12:07:22 -0700

    Created new Files for starting web app

---
+++ SpringTestWebFullApplication/src/main/java/GreetingPackage/GreetingController.java
@@ -0,0 +1,23 @@
+package GreetingPackage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping()
+    public String main(Map<String, Object> model){
+        model.put("someText", "Hi, this is start Page of my new WebApp!");
+        return "main";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required = false, defaultValue = "World") String name, Map<String, Object> model) {
+        model.put("name", name);
+        return "greeting";
+    }
+
+}
