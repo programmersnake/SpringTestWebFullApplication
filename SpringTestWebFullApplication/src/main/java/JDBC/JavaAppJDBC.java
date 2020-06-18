9401f36b71ad2742b93d5efa9f8f2ca59656dc7c
Kostin Denis <kostindenis2001@gmail.com>
2020-06-18 10:08:22 -0700

    Update JavaAppJDBC.java

--- SpringTestWebFullApplication/src/main/java/JDBC/JavaAppJDBC.java
+++ SpringTestWebFullApplication/src/main/java/JDBC/JavaAppJDBC.java
@@ -3,6 +3,7 @@ package JDBC;
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JavaAppJDBC {
 
@@ -10,8 +11,12 @@ public class JavaAppJDBC {
     static final String USER = "postgres";
     static final String PASS = "1";
 
-    public static void main(String[] argv) {
+    public static void main(String[] argv) throws SQLException {
+        createDbUserTable();
+    }
 
+
+    private static Connection getDBConnection(){
         System.out.println("Testing connection to PostgreSQL JDBC");
 
         try {
@@ -19,7 +24,6 @@ public class JavaAppJDBC {
         } catch (ClassNotFoundException e) {
             System.out.println("PostgreSQL JDBC Driver is not found. Include it in your library path ");
             e.printStackTrace();
-            return;
         }
 
         System.out.println("PostgreSQL JDBC Driver successfully connected");
@@ -32,13 +36,44 @@ public class JavaAppJDBC {
         } catch (SQLException e) {
             System.out.println("Connection Failed");
             e.printStackTrace();
-            return;
         }
 
         if (connection != null) {
             System.out.println("You successfully connected to database now");
+            return connection;
         } else {
             System.out.println("Failed to make connection to database");
+            return null;
         }
     }
+
+    private static void createDbUserTable() throws SQLException {
+        Connection dbConnection = null;
+        Statement statement = null;
+
+        String createTableSQL = "CREATE TABLE DBUSER("
+                + "USER_ID NUMBER(5) NOT NULL, "
+                + "USERNAME VARCHAR(20) NOT NULL, "
+                + "CREATED_BY VARCHAR(20) NOT NULL, "
+                + "CREATED_DATE DATE NOT NULL, " + "PRIMARY KEY (USER_ID) "
+                + ")";
+
+        try {
+            dbConnection = getDBConnection();
+            statement = dbConnection.createStatement();
+
+            statement.execute(createTableSQL);
+            System.out.println("Table \"dbuser\" is created!");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (statement != null) {
+                statement.close();
+            }
+            if (dbConnection != null) {
+                dbConnection.close();
+            }
+        }
+    }
+
 }
